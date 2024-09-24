af15247f030d500798348602fd02f53811b1c794
ThangVo0308 <thangvo0308@gmail.com>
2024-09-24 17:21:00 +0700

    add missing files

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/repositories/ReviewRepository.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/repositories/ReviewRepository.java
@@ -4,6 +4,9 @@ import org.jakartaee5g23.sportsfieldbooking.entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, String> {
+    List<Review> findBySportField_Id(String sportFieldID);
 }
