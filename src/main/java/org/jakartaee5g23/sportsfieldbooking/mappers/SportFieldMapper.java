2463d59946c1e00be8aba91cc0e54cdb4379fa51
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-10-09 11:04:26 +0700

    Refactor SportFieldMapper to ignore unnecessary fields in mapping

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/mappers/SportFieldMapper.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/mappers/SportFieldMapper.java
@@ -6,23 +6,46 @@ import org.jakartaee5g23.sportsfieldbooking.dtos.responses.sportField.SportField
 import org.jakartaee5g23.sportsfieldbooking.entities.SportField;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface SportFieldMapper {
-
     SportFieldMapper INSTANCE = Mappers.getMapper(SportFieldMapper.class);
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "fieldAvailabilities", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "rating", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "user", ignore = true)
     SportField toSportField(NewSportFieldRequest dto);
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "createdBy", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "fieldAvailabilities", ignore = true)
+    @Mapping(target = "rating", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "user", ignore = true)
     SportField toSportField(UpdateSportFieldRequest dto);
 
-    SportFieldResponse toSportFieldResponse(SportField entity);
+    @Mapping(target = "categoryName", source = "category.name")
+    @Mapping(target = "owner", expression = "java(UserMapper.INSTANCE.toUserResponse(sportField.getUser()))")
+    SportFieldResponse toSportFieldResponse(SportField sportField);
+
     @AfterMapping
     default void customizeDto(SportField entity, @MappingTarget SportFieldResponse dto) {
         dto.setCategoryName(entity.getCategory().getName());
         dto.setOwner(UserMapper.INSTANCE.toUserResponse(entity.getUser()));
     }
-
 }
