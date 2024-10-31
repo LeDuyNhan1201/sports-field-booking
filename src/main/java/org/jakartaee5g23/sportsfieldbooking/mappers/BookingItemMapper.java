c6564010c8d0597471139a3e90706e89172ed976
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-10-31 23:23:46 +0700

    feat: introduce BookingItemMapper and remove deprecated BookingItemsMapper

---
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/mappers/BookingItemMapper.java
@@ -0,0 +1,29 @@
+package org.jakartaee5g23.sportsfieldbooking.mappers;
+
+import org.jakartaee5g23.sportsfieldbooking.dtos.responses.booking.BookingItemResponse;
+import org.jakartaee5g23.sportsfieldbooking.dtos.responses.booking.BookingResponse;
+import org.jakartaee5g23.sportsfieldbooking.entities.Booking;
+import org.jakartaee5g23.sportsfieldbooking.entities.BookingItem;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
+public interface BookingItemMapper {
+
+        BookingItemMapper INSTANCE = Mappers.getMapper(BookingItemMapper.class);
+
+        BookingItemResponse toBookingItemResponse(BookingItem entity);
+
+        @AfterMapping
+        default void customizeDto(Booking entity, @MappingTarget BookingResponse dto) {
+                dto.setMUser(UserMapper.INSTANCE.toUserResponse(entity.getUser()));
+                dto.setMSportField(
+                                SportsFieldMapper.INSTANCE
+                                                .toSportsFieldResponse(entity.getFieldAvailability().getSportsField()));
+                dto.setMFieldAvailability(
+                                FieldAvailabilityMapper.INSTANCE
+                                                .toFieldAvailabilityResponse(entity.getFieldAvailability()));
+        }
+}
