06f4e3c9046f34464e3d92ba34f5f9c732e12cf4
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-11-14 10:52:26 +0700

    feat: add methods to mark notifications as read and find by ID

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/repositories/NotificationRepository.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/repositories/NotificationRepository.java
@@ -5,11 +5,29 @@ import org.jakartaee5g23.sportsfieldbooking.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import jakarta.transaction.Transactional;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     Page<Notification> findByUser(User user, Pageable pageable);
 
+    @Modifying
+    @Transactional
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.user = :user AND n.isRead = false")
+    void readAllNotifications(User user);
+
+    // find notifications by id string
+    @Query("SELECT n FROM Notification n WHERE n.id = :id")
+    Notification findById(String id);
+
+    // update status is read of notification
+    @Modifying
+    @Transactional
+    @Query("UPDATE Notification n SET n.isRead = true WHERE n.id = :id AND n.isRead = false")
+    void readNotification(String id);
 }
