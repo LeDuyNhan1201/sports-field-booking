ee457a581d4ec1bf65eac74fafb8fe18ab3a079e
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-11-15 23:12:01 +0700

    feat: modify CategoryServiceImpl to implement pagination in findAll method

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/services/impls/CategoryServiceImpl.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/services/impls/CategoryServiceImpl.java
@@ -1,27 +1,25 @@
 package org.jakartaee5g23.sportsfieldbooking.services.impls;
 
-import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
-
-import java.util.*;
-
 import org.jakartaee5g23.sportsfieldbooking.entities.Category;
 import org.jakartaee5g23.sportsfieldbooking.repositories.CategoryRepository;
 import org.jakartaee5g23.sportsfieldbooking.services.CategoryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @Slf4j
-public class CategoryServiceImpl implements CategoryService{
-    CategoryRepository categoryRepository;
+public class CategoryServiceImpl implements CategoryService {
+    final CategoryRepository categoryRepository;
+
     @Override
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public Page<Category> findAll(Pageable pageable) {
+        return categoryRepository.findAll(pageable);
     }
-    
 }
