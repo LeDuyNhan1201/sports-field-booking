c46f813f511c990bb2c6f168716b908c4466e4bc
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-11-15 17:18:46 +0700

    refactor: remove unused notification types from NotificationType enum

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/enums/NotificationType.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/enums/NotificationType.java
@@ -1,11 +1,8 @@
 package org.jakartaee5g23.sportsfieldbooking.enums;
 
 public enum NotificationType {
-    BOOKING_REMINDER,
-    PAYMENT_REMINDER,
     PROMOTION,
     ORDER_STATUS_UPDATE,
-    PAYMENT_STATUS_UPDATE,
     YARD_STATUS_UPDATE,
     COMMENT_FEEDBACK,
 }
