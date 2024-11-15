eb684f12b90a170e5010fa99b02f4b7d4c426b60
HoangGiaBao212 <hoanggiabao2122003@gmail.com>
2024-11-15 23:11:55 +0700

    feat: update CategoryService to support pagination in findAll method

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/services/CategoryService.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/services/CategoryService.java
@@ -1,13 +1,9 @@
 package org.jakartaee5g23.sportsfieldbooking.services;
 
-import java.util.List;
-
 import org.jakartaee5g23.sportsfieldbooking.entities.Category;
-import org.jakartaee5g23.sportsfieldbooking.entities.User;
-import org.jakartaee5g23.sportsfieldbooking.enums.SportsFieldStatus;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface CategoryService {
-    List<Category> findAll();
-    
-} 
+    Page<Category> findAll(Pageable pageable);
+}
