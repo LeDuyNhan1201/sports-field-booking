3f9a1715f6f4756ca571ba5ecae13061092005bd
ThangVo0308 <thangvo0308@gmail.com>
2024-11-18 00:04:54 +0700

    refactor: field availability

--- src/main/java/org/jakartaee5g23/sportsfieldbooking/controllers/FieldAvailabilityController.java
+++ src/main/java/org/jakartaee5g23/sportsfieldbooking/controllers/FieldAvailabilityController.java
@@ -1,7 +1,6 @@
 package org.jakartaee5g23.sportsfieldbooking.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -24,11 +23,8 @@ import org.jakartaee5g23.sportsfieldbooking.services.SportsFieldService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -53,17 +49,17 @@ public class FieldAvailabilityController {
 
     @Operation(summary = "Create new field availability", description = "Create a new field when the field manager wants to use the system", security = @SecurityRequirement(name = "bearerAuth"))
     @PostMapping("/create")
-    @PostAuthorize("returnObject.body.MUser.id == authentication.name")
+//    @PostAuthorize("returnObject.body.MUser.id == authentication.name")
     public ResponseEntity<Void> create(@RequestBody @Valid NewFieldAvailabilityRequest request) {
-            SportsField sportsField = sportsFieldService.findById(request.sportsFieldId());
-            FieldAvailability fieldAvailability = FieldAvailability.builder()
+        SportsField sportsField = sportsFieldService.findById(request.sportsFieldId());
+        FieldAvailability fieldAvailability = FieldAvailability.builder()
                 .startTime(request.startTime())
                 .endTime(request.endTime())
                 .price(request.price())
                 .sportsField(sportsField)
                 .build();
-            fieldAvailability.setSportsField(sportsField);
-            fieldAvailabilityService.create(fieldAvailability, request.isConfirmed());
-            return ResponseEntity.status(HttpStatus.CREATED).build();
+        fieldAvailability.setSportsField(sportsField);
+        fieldAvailabilityService.create(fieldAvailability, request.isConfirmed());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
